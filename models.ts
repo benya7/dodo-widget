9517dc2560a7b459faf517b27b45866910dfb473
en0c-026 <bitup.games@gmail.com>
2021-07-06 14:10:03 -0300

    update package.json

---
+++ models.ts
@@ -0,0 +1,65 @@
+interface InfraConfigurations {
+    element?: Element;
+}
+
+export interface AppConfigurations {
+    debug: boolean;
+    dodoBaseUrl: string;
+    cGeckoBaseUrl: string;
+    targetId: string;
+}
+
+export type Configurations = InfraConfigurations & AppConfigurations;
+
+export interface INetwork {
+    id: number;
+    chainId: string;
+    nameNet: string;
+    name: string;
+    rpc: string;
+    symbol: string;
+    explorer: string;
+}
+
+export interface DodoResponse {
+    status: number;
+    data: {
+        resAmount: string;
+        resPricePerToToken: string;
+        resPricePerFromToken: string;
+        priceImpact: string;
+        targetApproveAddr: string;
+        to: string;
+        data: string;
+    }
+}
+export interface DodoRequest {
+    fromTokenAddress: string;
+    fromTokenDecimals: number;
+    toTokenAddress: string;
+    toTokenDecimals: number;
+    fromAmount: string;
+    slippage: number;
+    userAddr: string;
+    chainId: number;
+    rpc: string;
+    deadLine?: number;
+    source?: string;
+}
+export interface DodoApi {
+    getDodoRoute: (params: DodoRequest) => Promise<DodoResponse>;
+}
+
+export interface INetworkWallet {
+    chainId: string;
+    blockExplorerUrls?: string[];
+    chainName?: string;
+    iconUrls?: string[];
+    nativeCurrency?: {
+        name: string;
+        symbol: string;
+        decimals: number;
+    };
+    rpcUrls?: string[];
+}
+export type IRequestParams = Array<INetworkWallet>
