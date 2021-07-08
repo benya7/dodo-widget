6230179af5aa30bcd0ba4f1988cbbacd15bf706f
en0c-026 <bitup.games@gmail.com>
2021-07-07 23:07:16 -0300

    update styles

--- src/components/ModalConnect/UserAccount.tsx
+++ src/components/ModalConnect/UserAccount.tsx
@@ -1,17 +1,17 @@
 import { useWeb3React } from '@web3-react/core';
 import { h } from 'preact';
-import { Text, Box, Button } from "grommet";
+import { Anchor, Box, Button } from "grommet";
+import { useStore } from '../../hooks';
 
 const UserAccount = () => {
     const { account, deactivate } = useWeb3React()
+    const { explorerUrl } = useStore();
     return (
         <Box direction='row' alignSelf='end' align='center' justify='center' gap='small'>
-            <Text size='small'>
-                {
+            <Anchor size='small' href={`${explorerUrl}/address/${account}`} label={
                     account ?
                         `${account.substring(0, 6)}...${account.substring(account.length - 4)}` : ''
-                }
-            </Text>
+                } />
             <Button size='small' label='Disconnect' onClick={() => {
                 deactivate()
                 window.location.reload()
